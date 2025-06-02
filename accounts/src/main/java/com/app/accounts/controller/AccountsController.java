52b6c53b34e4476fe1311178c2cab5900c7888e2
merveyilmaz <merveyilmaz@atmaca.com.tr>
2025-06-02 13:54:04 +0300

    add resilliencey patterns to my microservers

--- accounts/src/main/java/com/app/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/app/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.app.accounts.dto.CustomerDto;
 import com.app.accounts.dto.ErrorResponseDto;
 import com.app.accounts.dto.ResponseDto;
 import com.app.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -195,9 +196,16 @@ public class AccountsController {
                 .body("0.9");
     }
 
+    @RateLimiter(name= "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 21");
     }
 
     @GetMapping("/contact-info")
